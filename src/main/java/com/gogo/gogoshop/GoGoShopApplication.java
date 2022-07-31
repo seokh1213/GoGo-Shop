c681b26a79341189a6e21765bac191454441b1cf
seokh1213 <seokh1213@gmail.com>
2022-07-31 17:45:29 +0900

    init

---
+++ src/main/java/com/gogo/gogoshop/GoGoShopApplication.java
@@ -0,0 +1,13 @@
+package com.gogo.gogoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoGoShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gogo/gogoshop/GoGoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogo.gogoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
