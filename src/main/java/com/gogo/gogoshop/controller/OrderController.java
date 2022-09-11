9f3c0c0c7806d84f08422a362d3c7bf34d26464a
seokh1213 <seokh1213@gmail.com>
2022-09-11 17:40:34 +0900

    feature: change frontend url

--- src/main/java/com/gogo/gogoshop/controller/OrderController.java
+++ src/main/java/com/gogo/gogoshop/controller/OrderController.java
@@ -1,9 +1,20 @@
 package com.gogo.gogoshop.controller;
 
+import com.gogo.gogoshop.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/order")
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
+
+    @PostMapping
+    public BaseResponse orderProduct() {
+
+        return BaseResponse.of(null, 0);
+    }
+
 }
